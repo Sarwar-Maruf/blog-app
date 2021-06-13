9fbadd78c535561a0ce7c547159ef07a01fb10cc
sarwar_maruf <sarwar.maruf9@gmai.com>
2021-06-13 12:19:12 +0600

    initial commit

---
+++ oop.py
@@ -0,0 +1,39 @@
+# class Person:
+#     name = ""
+#     age = ""
+#     fname = ""
+#     mname = ""
+#     phone = ""
+
+#     def set_value(self,name,age,phone,fname,mname):
+#         self.name = name
+#         self.age = age
+#         self.phone = phone
+#         self.fname = fname
+#         self.mname = mname
+    
+#     def details(self):
+#         print(f"\nName:{self.name}\nAge:{self.age}\nPhone:{self.phone}\nFather Name :{self.fname}\nMother Name:{self.mname}\n")
+    
+
+
+# jim = Person()
+# jim.set_value("Sabbir Shihab Jim",18,"01774669401", "Ferdous Shah Alam","Sultana Parvin")
+# jim.details()
+
+# jebon =Person()
+# jebon.set_value("sarwar Maruf Jebon", 24, '01710144505' , 'ferdous shah alam', 'sultana parvin')
+# jebon.details()
+
+ 
+# class Triangel:
+#     def __init__(self,base,height):
+#          self.base = base
+#          self.height = height
+#     def calcualte_area(self):
+#         print(f"Area = {.5*float(self.height)*float(self.base)}")
+
+# a=Triangel(10,20)
+# b=Triangel(20,30)
+# a.calcualte_area()
+# b.calcualte_area()
